489f60bbef49b786c8a235307a00a9ed52a80a86
mikey-garcia <mgarcia75@gatech.edu>
2020-09-14 02:18:47 -0400

    heights and burn_notice started.
    Still need to test out both of them, add animations to burn_notice

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -37,6 +37,40 @@ def surveillance(robot: cozmo.robot.Robot):
         robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
         robot.turn_in_place(degrees(90)).wait_for_completed()
 
+def heights(robot: cozmo.robot.Robot):
+    '''
+    Drive robot in 'S' pattern (top to bottom, right to left)
+    Display animation on robot's face.
+
+    (Not yet tested)
+    '''
+    robot.drive_wheels(50,60,duration=1) # drive in straight-ish line to the left
+    robot.drive_wheels(30,60,duration=1) # drive in curved arc to the left
+    robot.drive_wheels(60,50,duration=3) # drive in straight-ish line to the right
+    robot.drive_wheels(60,30,duration=1)  # drive in curved arc to the right
+    robot.stop_all_motors()
+    robot.play_anim_trigger(
+        cozmo.anim.Triggers.CodeLabWin).wait_for_completed()  # play animation
+
+def burn_notice(robot: cozmo.robot.Robot):
+    '''
+    Drive in a square (20cm x 20cm), slowly/continuously lower and raise the lift (2s),
+    and state "I am not a spy". At end, lower the lift and return to idle state.
+
+    (Not yet tested, also add animation)
+    '''
+    def drive_side():
+        robot.move_lift(1.78)
+        robot.drive_straight(distance_mm(200), speed_mmps(50), in_parallel=True).wait_for_completed()
+        robot.turn_in_place(degrees(90)).wait_for_completed()
+        return
+
+    for i in range(4):
+        drive_side()
+
+    robot.LiftPosition(ratio = 0)
+
+
 def fsm(robot: cozmo.robot.Robot):
     img_clf = imgclassification_sol.ImageClassifier()
     (train_raw, train_labels) = img_clf.load_data_from_folder('./train/')
@@ -76,6 +110,10 @@ def fsm(robot: cozmo.robot.Robot):
             robot.say_text(predicted_label[0]).wait_for_completed()
             if(predicted_label == 'order'):
                 defuse(robot)
+            elif(predicted_label == 'drone'):
+                heights(robot)
+            elif(predicted_label == 'inspection'):
+                burn_notice(robot)
 
         time.sleep(2)
         print(predicted_label)
