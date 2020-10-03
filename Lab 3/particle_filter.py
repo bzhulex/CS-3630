e46c715554f9af32b4dc7fb2935fcfc43177dd59
mikey-garcia <mgarcia75@gatech.edu>
2020-10-03 19:31:34 -0400

    measurement_update initial changes
    added some stuff for measurement update

--- Lab 3/particle_filter.py
+++ Lab 3/particle_filter.py
@@ -56,7 +56,93 @@ def measurement_update(particles, measured_marker_list, grid):
         Returns: the list of particles represents belief p(x_{t} | u_{t})
                 after measurement update
     """
-    measured_particles = particles
+    def gaussian_prob_density(robot_reading, particle_reading):
+        '''
+        Find: d = dist_bt_markers
+              a = angle_bt_markers
+
+        Return probability
+        P(x) = exp(-(d**2)/(2sigma**2) + (angle**2)/(2sigma**2))
+        '''
+        # set the 2*sigma^2 constant for distance and angle
+        const_d = 2 * setting.MARKER_TRANS_SIGMA**2
+        const_a = 2 * setting.MARKER_ROT_SIGMA**2
+
+        # calculate dist and angle
+        d = grid_distance(robot_reading[0],robot_reading[1],particle_reading[0],particle_reading[1])
+        a = diff_heading_deg(robot_reading[2],particle_reading[2])
+
+        # calculate probability from d and a
+        prob = np.exp(-(d**2)/const_d + (a**2)/const_a)
+        return prob
+
+    def update_weights(particles,measured_marker_list, grid):
+        '''
+        Obtain real robot sensor readings, z_r
+
+        For each particle, simulate the particles FOV and obtain list of marker robot WOULD see
+        if really were at the pose z_pi
+
+        Compare z_pi and z_r and assign a weight to the particle proportional to how well they match
+        Comparison will be on distance and angle.
+        Comparison loop:
+            𝑝𝑟𝑜𝑏= 1.0; 
+            for each landmark 
+                𝑑= Euclidean distance to landmark 
+                𝑝𝑟𝑜𝑏*= Gaussian probability of obtaining a reading at distance 𝑑 
+                for this landmark from this particle 
+            return 𝑝𝑟𝑜𝑏
+        '''
+        # init weights
+        init_weight = 0.1 #some weight that all particles will get if no landmarks found by robot
+        weights = [init_weight]*len(particles)
+    
+        if len(measured_marker_list) == 0:
+            return weights
+
+        # calculate prob for each particle and store in weights list
+        for particle in particles:
+            prob = 1 # init as 1
+            particle_readings = particle.read_markers(grid)
+
+            if len(particle_readings) == 0:
+                weights.append(0) # if particle sees no landmarks, that particle gets a 0
+
+            else: 
+                # otherwise calculate the weights
+                for landmark in measured_marker_list:
+                    for particle_reading in particle_readings:
+                        prob*=gaussian_prob_density(landmark, particle_reading) #adjust probability
+                weights.append(prob)
+
+        return weights
+
+    def resample(particles, weights,grid):
+        '''
+        Generate new set of n particles
+        - Normalize them (divide each particle weight by sum of weights)
+        - Generate new particle distro based on probability equal to above normalized prob
+        - Throw out the low ones and replace with random sampling
+        - Maintain some small percentage of random samples
+        - Throw out all particles and start with uniform distro if all of them are unlikely (but not needed)
+        '''
+        # first normalize particle weights
+        SUM_WEIGHTS = sum(weights)
+        normalized_weights = [weight/SUM_WEIGHTS for weight in weights]
+        
+        # next generate new particle distribution based on above probabilities
+        threshold = 0.1 # anything less than this will be eliminated and replaced randomly
+        for i in range(len(particles)):
+            if normalized_weights[i] < threshold:
+                # eliminate very low prob. particle and replace with rand samples
+                particles[i] = particles[i].create_random(1,grid)[0] # get x,y,heading=None = random.uniform(0,360)
+        
+        return particles
+
+    # now we combine everything :)    
+    weights = update_weights(particles, measured_marker_list, grid)
+    measured_particles = resample(particles, weights, grid)
+
     return measured_particles
 
 
